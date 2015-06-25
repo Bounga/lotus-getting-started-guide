beaba0676f958b9bdcac4865b514244c61c98445
Nicolas Cavigneaux <nico@bounga.org>
2015-06-25 18:27:18 +0200

    Add book creation form

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,3 +1,5 @@
+post '/books', to: 'books#create'
+get '/books/new', to: 'books#new'
 get '/books', to: 'books#index'
 get '/', to: 'home#index'
 # Configure your routes here
---
+++ apps/web/controllers/books/create.rb
@@ -0,0 +1,12 @@
+module Web::Controllers::Books
+  class Create
+    include Web::Action
+
+    def call(params)
+      book = Book.new(params[:book])
+      BookRepository.create book
+
+      redirect_to '/books'
+    end
+  end
+end
