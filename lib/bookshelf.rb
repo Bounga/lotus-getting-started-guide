71f50bacd8f8c4d2c78f3f25678ce8c04b20e09a
Nicolas Cavigneaux <nico@bounga.org>
2015-06-25 10:54:56 +0200

    Add book entity

--- lib/bookshelf.rb
+++ lib/bookshelf.rb
@@ -35,12 +35,15 @@ Lotus::Model.configure do
   # Alternatively, you can use a block syntax like the following:
   #
   mapping do
-    # collection :users do
-    #   entity     User
-    #   repository UserRepository
-    #
-    #   attribute :id,   Integer
-    #   attribute :name, String
-    # end
+    collection :books do
+      entity     Book
+      repository BookRepository
+
+      attribute :id,   Integer
+      attribute :title, String
+      attribute :author, String
+      attribute :created_at, DateTime
+      attribute :updated_at, DateTime
+    end
   end
 end.load!
